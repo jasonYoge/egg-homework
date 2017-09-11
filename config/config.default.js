7e6ff22708303b528b530bedeca2aa94b6a24b98
ywj <ywj@ywjdeMacBook-Pro.local>
2017-09-11 22:26:17 +0800

    feat: Init project.

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_jasonyoung';
+
+  // add your config here
+
+  return config;
+};
